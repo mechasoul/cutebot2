27770a833306c23c015cc08f6f14c39cfdccd98a
mecha <mechasoul@mechasoulindustries.gov.ca>
2020-03-11 19:52:14 -0700

    just include the quote itself in the quote info file

--- cutebot/src/main/java/my/cute/bot/commands/TextChannelQuoteCommand.java
+++ cutebot/src/main/java/my/cute/bot/commands/TextChannelQuoteCommand.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Random;
@@ -24,10 +25,30 @@ import net.dv8tion.jda.api.entities.Message;
 
 public class TextChannelQuoteCommand extends TextChannelCommand {
 	
+	/*
+	 * TODO
+	 * a lot of static patterns here maybe should rethink having a separate
+	 * instance of this class in every guild's commandset
+	 * alternatively could allow for guilds to build their own quote file 
+	 * just right now everything is shared on a single quote file so this
+	 * entire class could be made singleton with no issue
+	 */
+	
 	private static final Logger logger = LoggerFactory.getLogger(TextChannelQuoteCommand.class);
 
+	/*
+	 * the file that holds quotes
+	 * each line should be a single quote
+	 */
 	private static final Path QUOTES_FILE = Paths.get("./twitchchat.txt");
-	private static final Path QUOTE_INFO_FILE = Paths.get("./quotes_info.txt");
+	/*
+	 * the file that saves the daily quote
+	 * first line is the datestamp of the saved quote 
+	 * (as by LocalDate.format(DateTimeFormatter.BASIC_ISO_DATE)
+	 * second line is the actual quote of the day
+	 */
+	private static final Path DAILY_QUOTE_FILE = Paths.get("./daily_quote.txt");
+	private static final Random RAND = new Random();
 	
 	TextChannelQuoteCommand(String name) {
 		super(name);
@@ -36,20 +57,26 @@ public class TextChannelQuoteCommand extends TextChannelCommand {
 	@Override
 	public void execute(Message message) {
 		String dailyQuote = null;
-		try (BufferedReader reader = Files.newBufferedReader(QUOTE_INFO_FILE, StandardCharsets.UTF_8)) {
-			if(!isExpired(reader.readLine())) {
-				//no update needed. return current day's quote
-				dailyQuote = this.getDailyQuote(Integer.parseInt(reader.readLine()));
+		/*
+		 * prevent possibly having two guilds try to update quote file at same time,
+		 * read when quote isn't done writing, etc
+		 */
+		synchronized(TextChannelQuoteCommand.class) {
+			try (BufferedReader reader = Files.newBufferedReader(DAILY_QUOTE_FILE, StandardCharsets.UTF_8)) {
+				if(!isExpired(reader.readLine())) {
+					//no update needed. return current day's quote
+					dailyQuote = reader.readLine();
+				}
+			} catch (NoSuchFileException e) {
+				//likely first run, do nothing. will continue and create quote info file and get new quote
+			} catch (IOException e) {
+				logger.warn(this + ": exception thrown in execute(): " + e.getMessage(), e);
+				dailyQuote = "the quotes broke so im taking the day off";
 			}
-		} catch (NoSuchFileException e) {
-			//likely first run, do nothing. will continue and create quote info file and get new quote
-		} catch (IOException e) {
-			logger.error("exception thrown in TextChannelQuoteCommand.execute(): " + e.getMessage(), e);
-			dailyQuote = "the quotes broke so im taking the day off";
 		}
 		if(dailyQuote == null) {
 			//need to update quote info file and get new quote
-			dailyQuote = this.updateQuoteInfoFile();
+			dailyQuote = this.updateDailyQuoteFile();
 		}
 		message.getChannel().sendMessage(new MessageBuilder().append("today's Twitch Chat:tm: Quote Of The Day (!quote)")
 				.setEmbed(new EmbedBuilder().setDescription(dailyQuote).build()).build()).queue();
@@ -76,40 +103,40 @@ public class TextChannelQuoteCommand extends TextChannelCommand {
 	 * given date and LocalDate.now()), and false if not
 	 */
 	private boolean isExpired(String dateStamp) {
-		return ChronoUnit.DAYS.between(LocalDate.parse(dateStamp, DateTimeFormatter.BASIC_ISO_DATE), LocalDate.now()) >= 1;
+		try {
+			return ChronoUnit.DAYS.between(LocalDate.parse(dateStamp, DateTimeFormatter.BASIC_ISO_DATE), LocalDate.now()) >= 1;
+		} catch (DateTimeParseException e) {
+			//something weird happened. should remake daily quote
+			logger.warn(this + ": exception in parsing dateStamp '" + dateStamp + "', rebuilding "
+					+ "daily quote file. ex: " + e, e);
+			return true;
+		}
 	}
 	
-	private String getDailyQuote(int lineNum) {
-		String quote = null;
-		try (BufferedReader quoteReader = Files.newBufferedReader(QUOTES_FILE, StandardCharsets.UTF_8)) {
-			while(lineNum >= 0) {
-				quote = quoteReader.readLine();
-				lineNum--;
+	/*
+	 * updates the DAILY_QUOTE_FILE
+	 * writes the current datestamp to the file, as given by
+	 * LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE
+	 * and then retrieves a random line from QUOTES_FILE, which
+	 * is selected as the daily quote, and writes it to DAILY_QUOTE_FILE
+	 * 
+	 * returns the selected daily quote
+	 */
+	private String updateDailyQuoteFile() {
+		synchronized(TextChannelQuoteCommand.class) {
+			try (BufferedWriter writer = Files.newBufferedWriter(DAILY_QUOTE_FILE, StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+					StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
+				writer.append(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
+				writer.newLine();
+				//get all quotes and randomly assign one as daily quote
+				List<String> quotes = Files.readAllLines(QUOTES_FILE, StandardCharsets.UTF_8);
+				String dailyQuote = quotes.get(RAND.nextInt(quotes.size()));
+				writer.append(dailyQuote);
+				return dailyQuote;
+			} catch (IOException e) {
+				logger.warn(this + ": exception thrown in updateQuoteInfoFile(): " + e.getMessage(), e);
+				return "the quotes broke so im taking the day off";
 			}
-		} catch (IOException e) {
-			logger.error("exception encountered in TextChannelQuoteCommand.getDailyQuote(): " + e.getMessage(), e);
-		}
-		if(quote != null) {
-			return quote;
-		} else {
-			logger.error("getDailyQuote() returned a null quote!");
-			return "the quotes broke so im taking the day off";
-		}
-	}
-	
-	private String updateQuoteInfoFile() {
-		try (BufferedWriter writer = Files.newBufferedWriter(QUOTE_INFO_FILE, StandardCharsets.UTF_8, StandardOpenOption.CREATE,
-				StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
-			writer.append(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
-			writer.newLine();
-			//get all quotes and randomly assign one as daily quote
-			List<String> quotes = Files.readAllLines(QUOTES_FILE, StandardCharsets.UTF_8);
-			int quoteNumber = new Random().nextInt(quotes.size());
-			writer.append(""+quoteNumber);
-			return quotes.get(quoteNumber);
-		} catch (IOException e) {
-			logger.error("exception thrown in TextChannelQuoteCommand.updateQuoteInfoFile(): " + e.getMessage(), e);
-			return "the quotes broke so im taking the day off";
 		}
 	}
 }
